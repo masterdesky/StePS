e2f650b60f33353e1651aa2b9ab5a291dafa1488
masterdesky <masterdesky@gmail.com>
2025-01-19 00:29:38 +0100

    cleaned up functions for calculation power spectrum and growth function

--- StePS_IC/src/powerspec.py
+++ StePS_IC/src/powerspec.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-
 #*******************************************************************************#
 #  StePS_IC.py - An initial condition generator for                             #
 #     STEreographically Projected cosmological Simulations                      #
-#    Copyright (C) 2017-2024 Gabor Racz                                         #
+#    Copyright (C) 2017-2025 Gabor Racz                                         #
 #                                                                               #
 #    This program is free software; you can redistribute it and/or modify       #
 #    it under the terms of the GNU General Public License as published by       #
@@ -17,81 +15,132 @@
 #*******************************************************************************#
 
 import numpy as np
+
 import camb
-from camb import model, initialpower
-from camb.dark_energy import DarkEnergyFluid
+from colossus.cosmology import cosmology
 
-from colossus.cosmology import cosmology #Colossus is used for linear growth calculation
 
-def get_Linear_GrowthFunction(z, H0, OmegaM, OmegaB, OmegaL, s8, ns, DE, DE_params, SILENT=False):
+def linear_growth_function(z, H0, omega_m, omega_b, omega_l, s8, ns, DE, DE_params,
+                           silent=True):
     '''
-    For w0 and CPL dark energy, we use the Colossus implementation of Linder & Jenkins 2003 Eq. 11.
-    https://arxiv.org/pdf/astro-ph/0305286.pdf
+    Calculate the linear growth factor D(a) normalized to 1 at a=1.
+
+    For w0 and CPL dark energy, we use the Colossus implementation of
+    Eq. (11) from Linder & Jenkins (2003).
+    See paper at https://arxiv.org/pdf/astro-ph/0305286.pdf.
+
+    Returns:
+    --------
+    Dlin : float
+        The linear growth factor at a=1.
     '''
-    #Calculating the curvature
-    OmegaK = 1.0-OmegaM-OmegaL
-    if np.abs(OmegaK)<=1e-5:
-        flat=True
-        print("Flat cosmology.")
+    # Calculating the curvature
+    omega_k = 1.0 - omega_m - omega_l
+    if np.abs(omega_k) <= 1e-5:
+        flat = True
+        print('Flat cosmology.')
     else:
-        flat=False
-        print("Non-flat cosmology, OmegaK=%e, OmegaM=%.4f, OmegaL=%.4f" %(OmegaK, OmegaM, OmegaL))
-    Tcmb = 0.001 # Zero CMB temperature can cause issues in colossus. This small value shouldn't cause any significant errors at late times in relevant cosmologies. (non-zero OmegaR will be implemented in the future.)
+        flat = False
+        print(f'Non-flat cosmology; {omega_k =}, {omega_m =:.4f}, {omega_l =:.4f}')
+    # Zero CMB temperature can cause issues in colossus. This small value
+    # shouldn't cause any significant errors at late times in relevant
+    # cosmologies. (Non-zero `omega_r` will be implemented in the future.)
+    T_cmb = 0.001
+    
+    params = {  # Common cosmological parameters for all models
+        'flat': flat, 'H0': H0, 'Om0': omega_m, 'Ob0': omega_b,
+        'sigma8': s8, 'ns': ns, 'Tcmb0': T_cmb
+    }
     if DE == 'Lambda':
-        if flat==True:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'sigma8': s8, 'ns': ns, 'Tcmb0': Tcmb}
-        else:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'Ode0': OmegaL, 'sigma8': s8, 'ns': ns, 'Tcmb0': Tcmb}
+        if not flat:
+            params.update({'Ode0': omega_l})
         cosmo = cosmology.setCosmology('LCDM', **params)
     elif DE == 'w0':
-        if flat==True:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'sigma8': s8, 'ns': ns, 'de_model': 'w0', 'w0': DE_params[0], 'Tcmb0': Tcmb}
-        else:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'Ode0': OmegaL, 'sigma8': s8, 'ns': ns, 'de_model': 'w0', 'w0': DE_params[0], 'Tcmb0': Tcmb}
+        params.update({'de_model': 'w0', 'w0': DE_params[0]})
+        if not flat:
+            params.update({'Ode0': omega_l})
         cosmo = cosmology.setCosmology('wCDM', **params)
     elif DE == 'CPL':
-        if flat==True:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'sigma8': s8, 'ns': ns, 'de_model': 'w0wa', 'w0': DE_params[0], 'wa': DE_params[1], 'Tcmb0': Tcmb}
-        else:
-            params = {'flat': flat, 'H0': H0, 'Om0': OmegaM, 'Ob0': OmegaB, 'Ode0': OmegaL, 'sigma8': s8, 'ns': ns, 'de_model': 'w0wa', 'w0': DE_params[0], 'wa': DE_params[1], 'Tcmb0': Tcmb}
+        params.update({'de_model': 'w0wa', 'w0': DE_params[0], 'wa': DE_params[1]})
+        if not flat:
+            params.update({'Ode0': omega_l})
         cosmo = cosmology.setCosmology('w0waCDM', **params)
-    Dlin = cosmo.growthFactorUnnormalized(z)/cosmo.growthFactorUnnormalized(0.0)
-    if not SILENT:
-        print("Initial normalized linear growth: D(z=%.2f)/D(z=0) = %e" % (z, Dlin))
+    Dlin = cosmo.growthFactorUnnormalized(z) / cosmo.growthFactorUnnormalized(0.0)
+    if not silent:
+        print(f'Initial normalized linear growth: D(z={z:.2f})/D(z=0) = {Dlin:.2e}')
     return Dlin
 
-def get_CAMB_Linear_SPECTRUM(H0=73.0, ombh2=0.024, omch2=0.1092445, omk=0.0,ns=1.0,redshift=127,kmin=0.01,kmax=1.0,npoints=1024,sigma8='Auto',DE='Lambda',DE_params=None):
-    pars = camb.CAMBparams()
-    pars.set_cosmology(H0=H0, ombh2=ombh2, omch2=omch2, omk=omk)
-    if DE != 'Lambda':
-        if DE == 'w0':
-            pars.DarkEnergy = DarkEnergyFluid()
-            pars.DarkEnergy.set_params(w=DE_params[0])
-        elif DE == 'CPL':
-            pars.DarkEnergy = DarkEnergyFluid()
-            pars.DarkEnergy.set_params(w=DE_params[0],wa=DE_params[1])
-    pars.InitPower.set_params(ns=ns)
-    pars.set_matter_power(redshifts=[0.0], kmax=kmax)
-    #Linear spectra
-    pars.NonLinear = model.NonLinear_none
-    results = camb.get_results(pars)
+def linear_spectrum_camb(
+        z=127, H0=73.0, ombh2=0.024, omch2=0.1092445, omega_k=0.0, ns=1.0,
+        kmin=0.01, kmax=1.0, npoints=1024, sigma8=None, DE='Lambda', DE_params=None):
+    '''
+    Calculate the linear matter power spectrum using CAMB. The output is
+    normalized to the linear growth factor at z=0. The default cosmological
+    parameters are from the Planck 2018 results.
+
+    Parameters:
+    -----------
+    z : float
+        Redshift at which the linear power spectrum is calculated.
+    H0 : float
+        Hubble constant in km/s/Mpc.
+    ombh2 : float
+        Physical baryon density parameter.
+    omch2 : float
+        Physical cold dark matter density parameter.
+    omega_k : float
+        Curvature density parameter.
+    ns : float
+        Scalar spectral index.
+    kmin : float
+        Minimum wavenumber in h/Mpc.
+    kmax : float
+        Maximum wavenumber in h/Mpc.
+    npoints : int
+        Number of wavenumber points.
+    sigma8 : float; default=None
+        Rescale the linear power spectrum to the given sigma8 value.
+    DE : str
+        Dark energy model. Options are 'Lambda', 'w0', 'CPL'.
+    DE_params : list or array-like
+        Dark energy model parameters. For 'Lambda' and 'w0', it is a single
+        element list containing the dark energy equation of state parameter w.
+        For 'CPL', it is a two element list containing w and wa.
+    '''
+    params = camb.CAMBparams()
+    params.set_cosmology(H0=H0, ombh2=ombh2, omch2=omch2, omk=omega_k)
+    if DE == 'w0':
+        params.DarkEnergy = camb.dark_energy.DarkEnergyFluid()
+        params.DarkEnergy.set_params(w=DE_params[0])
+    elif DE == 'CPL':
+        params.DarkEnergy = camb.dark_energy.DarkEnergyFluid()
+        params.DarkEnergy.set_params(w=DE_params[0], wa=DE_params[1])
+    
+    # Calculate sigma_8 at z=0
+    params.NonLinear = camb.model.NonLinear_none
+    params.InitPower.set_params(ns=ns)
+    params.set_matter_power(redshifts=[0.0], kmax=kmax)
+    results = camb.get_results(params)
     s8 = results.get_sigma8()
-    print("original s8: %f"%s8)
-    pars.set_matter_power(redshifts=[0.0], kmax=kmax)
-    results = camb.get_results(pars)
-    s8 = results.get_sigma8()
-    #calculating normalized linear growth function
-    omm=(ombh2+omch2)/((H0/100.0)**2) #non-relativistic matter density
-    omb=ombh2/((H0/100.0)**2) #baryonic matter density
-    oml=1.0-omk-omm # dark energy density
-    DzD0 = get_Linear_GrowthFunction(redshift, H0, omm, omb, oml, s8, ns, DE, DE_params )
-    #calculating the z=0 linear P(k)
-    kh, z, pk = results.get_matter_power_spectrum(minkh=kmin, maxkh=kmax, npoints = npoints)
-    if sigma8 != 'Auto':
-        pars.InitPower.set_params(As=2e-09*(sigma8/s8)**2, ns=ns, nrun=0, nrunrun=0.0, r=0.0, nt=None, ntrun=0.0, pivot_scalar=0.05, pivot_tensor=0.05, parameterization='tensor_param_rpivot')
-        pars.set_matter_power(redshifts=[0.0], kmax=kmax)
-        results = camb.get_results(pars)
+    print(f'Original sigma_8: {s8}')
+
+    # Calculating normalized linear growth function
+    omega_m = (ombh2+omch2) / ((H0/100.0)**2)  # Non-relativistic matter density
+    omega_b = ombh2 / ((H0/100.0)**2)          # Baryonic matter density
+    omega_l = 1.0 - omega_k - omega_m          # Dark energy density
+    DzD0 = linear_growth_function(
+                z, H0, omega_m, omega_b, omega_l, s8, ns, DE, DE_params)
+    
+    # Calculating the linear P(k) at z=0
+    if sigma8 is None:
+        params.InitPower.set_params(
+                As=2e-09*(sigma8/s8)**2, ns=ns, nrun=0, nrunrun=0.0, r=0.0,
+                nt=None, ntrun=0.0, pivot_scalar=0.05, pivot_tensor=0.05,
+                parameterization='tensor_param_rpivot')
+        params.set_matter_power(redshifts=[0.0], kmax=kmax)
+        results = camb.get_results(params)
         s8 = results.get_sigma8()
-        print("rescaled s8: %f"%s8)
-        kh, z, pk = results.get_matter_power_spectrum(minkh=kmin, maxkh=kmax, npoints = npoints)
+        print(f'Rescaled sigma_8: {s8}')
+    kh, _, pk = results.get_matter_power_spectrum(
+                                minkh=kmin, maxkh=kmax, npoints=npoints)
     return kh, pk[0]*DzD0**2
