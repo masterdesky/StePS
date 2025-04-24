3c73c06ef83f7412d9dae366eb1fda3baa1a04e0
masterdesky <masterdesky@gmail.com>
2025-04-25 00:16:48 +0200

    reduced cosmodata to appropriate functions only

--- StePS_IC/stepsic/data.py
+++ StePS_IC/stepsic/data.py
@@ -17,34 +17,36 @@
 import numpy as np
 
 
-class CosmoSnapshot:
+class CosmoData:
     '''
     Implements a class for handling cosmological snapshot data stored in
     a conventional Gadget-like structure for StePS simulations.
 
     Parameters:
     -----------
-    snapshot : ndarray
-        Array containing the particle data.
+    data : ndarray of shape (N, 7)
+        Array containing the particle data, where N is the number of particles.
     idx : int, optional; default=0
-        Index of the particle ID column in the snapshot array.
+        Index of the particle ID column in the data array.
     cidx : tuple, optional; default=(1, 2, 3)
-        Indices of the x, y, z columns in the snapshot array.
+        Indices of the x, y, z columns in the data array.
     vidx : tuple, optional; default=(4, 5, 6)
-        Indices of the vx, vy, vz columns in the snapshot array.
+        Indices of the vx, vy, vz columns in the data array.
     midx : int, optional; default=6
-        Index of the mass column in the snapshot array.
+        Index of the mass column in the data array.
     '''
-    def __init__(self, snapshot, idx=0, cidx=(1, 2, 3), vidx=(4, 5, 6), midx=6,
+    def __init__(self, data, idx=0, cidx=(1, 2, 3), vidx=(4, 5, 6), midx=6,
                  silent=False):
-        self.snapshot = snapshot
-        self.idx, self.cidx, self.vidx, self.midx = idx, cidx, vidx, midx
+        self.id = data[:, idx]     # Particle IDs
+        self.pos = data[:, cidx]   # Particle positions
+        self.vel = data[:, vidx]   # Particle velocities
+        self.mass = data[:, midx]  # Particle masses
         self.silent = silent
     
         # Calculated values
-        self.N_part = snapshot.shape[0]  # Number of particles
-        self.mass_list = None            # List of unique particle masses
-        self.M_box = None                # Total mass in the box (in Msol)
+        self.N_part = data.shape[0]  # Number of particles
+        self.mass_list = None        # List of unique particle masses
+        self.M_box = None            # Total mass in the box (in Msol)
 
     def rescale_snapshot_mass(self, params):
         '''
@@ -55,22 +57,24 @@ class CosmoSnapshot:
         params : dict
             Dictionary containing the cosmological parameters.
         midx : int, optional; default=6
-            Index of the mass column in the snapshot array.
+            Index of the mass column in the data array.
         '''
-        M_tot = np.sum(self.snapshot[:, self.midx])
+        print("Rescaling the particle masses to fit the cosmological parameters...")
+        M_tot = np.sum(self.mass)
         V_sim = 4.0*np.pi/3.0 * params['RSIM']**3
         omegam_box = (M_tot / V_sim) / params['RHO_CRIT']
         if np.isclose(omegam_box, params['OMEGAM'], rtol=1e-9):
             print(f"The cosmological Omega_m parameter, calculated from \
                   the particle masses: Omega_m={omegam_box:.6f}")
         else:
-            self.snapshot[:, self.midx] *= params['OMEGAM'] / omegam_box
+            self.mass *= params['OMEGAM'] / omegam_box
             print(f"The particle masses were rescaled to fit with the \
                   cosmological parameter Omega_m={params['OMEGAM']}")
         # Calculate mass statistics after rescaling
-        self.mass_list = np.unique(self.snapshot[:, self.midx])
+        self.mass_list = np.unique(self.mass)
         print(f"Number of different masses:\t{len(self.mass_list)}")
         self.M_box = params['RHO_MEAN'] * params['LBOX']**3
+        print("...done.\n")
 
     def periodic_shift(self, params):
         '''
@@ -84,42 +88,6 @@ class CosmoSnapshot:
         '''
         print("Periodically shifting the input glass...")
         for i, vi in enumerate(['VOIX', 'VOIY', 'VOIZ']):
-            self.snapshot[:, self.cidx[i]] += params[vi]
-        self.snapshot[:, self.cidx] %= params['LBOX']
+            self.pos[i] += params[vi]
+        self.pos %= params['LBOX']
         print("...done.\n")
-
-    def create_mass_nsample_lut(self, params):
-        '''
-        Creates a lookup table for the number of samples per mass bin
-        for a variable resolution grid in a regular StePS simulation.
-
-        Parameters:
-        -----------
-        params : dict
-            Dictionary containing the cosmological parameters.
-        midx : int, optional; default=6
-            Index of the mass column in the snapshot array.
-        '''
-        if self.mass_list is None or self.M_box is None:
-            self.rescale_snapshot_mass(params)
-        
-        mass_nsample = np.c_[
-                self.mass_list,
-                np.uint32(np.cbrt(self.M_box / self.mass_list))
-        ]
-        mass_nsample_lut = np.zeros((params['NGRIDSAMPLES'], 2), dtype=np.uint32)
-        d_nsample = np.uint32(np.ceil(len(self.mass_list) / params['NGRIDSAMPLES']))
-        
-        print("The generated Nsample list:")
-        print("ID\tNsample\tMass(in 10e11Msol)")
-        # Populate lookup table by starting with the outermost mass bin,
-        # while skipping the innermost layer
-        mass_nsample_sorted = sorted(mass_nsample, key=lambda x: x[0])
-        for si in reversed(range(params['NGRIDSAMPLES']-1)):
-            if si == params['NGRIDSAMPLES']-2:
-                idx = 0
-            else:
-                idx = len(mass_nsample_sorted) - 1 - si*d_nsample
-            mass_nsample_lut[si] = mass_nsample_sorted[idx]
-            print(f"{si}\t{mass_nsample_lut[si, 0]}\t{mass_nsample_lut[si, 1]/1e11:.6f}")
-        self.mass_nsample_lut = mass_nsample_lut
