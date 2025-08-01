1a6fe55d19a97be44b0a975b3e4bb4697e49e1b8
masterdesky <masterdesky@gmail.com>
2025-08-02 00:53:55 +0200

    Implemented full parameter support
    - Added comprehensive validations and transformations for all known parameters.
    - Improved validation and error handling for input parameters.
    - Updated config documentation to reflect new parameter options and usage.
    - Added own config file for notebooks to avoid conflicts with main config.

--- StePS_IC/stepsic/parameters.py
+++ StePS_IC/stepsic/parameters.py
@@ -14,26 +14,30 @@
 #    GNU General Public License for more details.                               #
 #*******************************************************************************#
 
+from __future__ import annotations
 import importlib.resources
-from pathlib import Path
+
 import toml
 import numpy as np
+from pathlib import Path
 from textwrap import dedent
-
-from dataclasses import dataclass, field
 
 import logging
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-class CosmoParameters:
-    def __init__(self, filepath):
 
-        with open(filepath, 'r') as file:
+class CosmoParameters:
+    def __init__(self, path: str | Path):
+        with open(Path(path), 'r') as file:
             self.P = toml.load(file)
         self._load_default_cosmology(parameter_set=self.P['COSMOLOGY'])
         self._process_cosmo_params()
         self._process_ic_params()
+
+    def get_parameters(self):
+        '''Returns the parameters as a dictionary.'''
+        return self.P
     
     def _check_exists(self, name):
         '''Check if the parameter exists in the parameter list.'''
@@ -59,23 +63,32 @@ class CosmoParameters:
         value = self.P[name]
         if not isinstance(value, str):
             raise TypeError(f'{name} must be a string!')
-        return value.strip().lower()
+        self.P[name] = value.strip().lower()
+    
+    def _check_boolean(self, name):
+        '''Check if the parameter is a boolean value.'''
+        value = self.P[name]
+        if not isinstance(value, bool):
+            raise TypeError(f'{name} must be a boolean!')
 
     def _check_scalar(self, name, dtype=np.float64):
         '''Check if the parameter is a scalar value.'''
         value = self.P[name]
         if not np.isscalar(value):
             raise TypeError(f'{name} must be a scalar!')
-        return dtype(value)
+        self.P[name] = dtype(value)
 
-    def _check_array_or_scalar(self, name, length=3, dtype=np.float64):
-        '''Check if the parameter is a scalar or a container of a given length.'''
+    def _check_array_or_scalar(self, name, length:int = 3, dtype=np.float64):
+        '''
+        Check if the parameter is a scalar or a container of a given length.
+        If it is a scalar, convert it to an array of the specified length.
+        '''
         value = self.P[name]
         if not (np.isscalar(value) or (hasattr(value, '__len__') and len(value) == length)):
             raise TypeError(f'{name} must be a scalar or a container of {length} elements!')
         if not np.issubdtype(np.asarray(value).dtype, np.number):
             raise TypeError(f'{name} must be a scalar or numeric array!')
-        return np.broadcast_to(value, (length,)).astype(dtype)
+        self.P[name] = np.broadcast_to(value, (length,)).astype(dtype)
 
     def _load_default_cosmology(self, *,
             parameter_set='Planck2018EE+BAO+SN', value_type='best'):
@@ -83,8 +96,8 @@ class CosmoParameters:
         if value_type not in ['best', 'mean']:
             raise ValueError("The 'value_type' must be either 'best' or 'mean'!")
 
-        with importlib.resources.path('stepsic.config', 'cosmology.toml') as filepath:
-            with open(filepath, 'r') as f:
+        with importlib.resources.path('stepsic.config', 'cosmology.toml') as path:
+            with open(path, 'r') as f:
                 config_data = toml.load(f)
 
         if parameter_set not in config_data:
@@ -106,6 +119,14 @@ class CosmoParameters:
         '''
         Print all cosmological parameters found in the parameter list.
         '''
+        __COSMO_PARAMS = [
+            'H0', 'OMEGA_M', 'OMEGA_B', 'OMEGA_L', 'NS', 'AS',
+            'SIGMA8', 'YHE', 'MNU', 'NNU', 'ZREI', 'TCMB',
+            'KPIVOT', 'W0', 'WA'
+        ]
+        for key in __COSMO_PARAMS:
+            self._check_scalar(key)
+
         self.P['H'] = self.P['H0'] / 100.0
         self.P['OMMH2'] = self.P['OMEGA_M'] * self.P['H']**2
         self.P['OMBH2'] = self.P['OMEGA_B'] * self.P['H']**2
@@ -113,13 +134,11 @@ class CosmoParameters:
         self.P['OMEGA_C'] = self.P['OMEGA_M'] - self.P['OMEGA_B'] - self.P['OMEGA_NU']
         self.P['OMCH2'] = self.P['OMEGA_C'] * self.P['H']**2
         self.P['OMEGA_K'] = 1.0 - self.P['OMEGA_M'] - self.P['OMEGA_L']
-        self.P['SCALE'] = 1.0 / (self.P['REDSHIFT'] + 1.0)
 
-        # Print cosmological parameters
         text = dedent(f'''
         Cosmological Parameters:
         ------------------------
-        H0:                {self.P['H0']:.3f} km/s/Mpc
+        H0:                {self.P['H0']:.3f} km s^-1 Mpc^-1
         Omega_m:           {self.P['OMEGA_M']:.6f}
         Omega_m h^2:       {self.P['OMMH2']:.6f}
         Omega_c h^2:       {self.P['OMCH2']:.6f}
@@ -127,55 +146,81 @@ class CosmoParameters:
         Omega_k:           {self.P['OMEGA_K']:.6f}
         Omega_b:           {self.P['OMEGA_B']:.6f}
         Omega_b h^2:       {self.P['OMBH2']:.6f}
+        n_s:               {self.P['NS']:.6f}
+        A_s:               {self.P['AS']:.6e}
         Sigma8:            {self.P['SIGMA8']:.3f}
-        Redshift:          {self.P['REDSHIFT']:.3f}
-        Scale factor:      {self.P['SCALE']:.6f}
+        Omega_nu:          {self.P['OMEGA_NU']:.6f}
+        M_nu:              {self.P['MNU']:.3f} eV
+        N_nu:              {self.P['NNU']:.3f}
+        Y_He:              {self.P['YHE']:.6f}
+        z_reionization:    {self.P['ZREI']:.3f}
+        T_CMB:             {self.P['TCMB']:.3f} K
+        k_pivot:           {self.P['KPIVOT']:.3f} Mpc^-1
+        w0:                {self.P['W0']:.3f}
+        wa:                {self.P['WA']:.3f}
         ''')
         log.info(text)
-    
-        SPECTRUM = self._check_string('SPECTRUM')
-        if SPECTRUM not in ['input', 'camb']:
-            raise ValueError(f'Error: unknown spectrum type `{SPECTRUM}`!\nExiting.')
-        if SPECTRUM == 'input':
-            _ = self._check_path('FILE_WITH_INPUT_SPECTRUM')
-        _ = self._check_scalar('INPUTSPECTRUM_UNITLENGTH_IN_CM')
 
     def _process_ic_params(self):
         '''Process initial condition parameters from the config file.'''
-        _ = self._check_array_or_scalar('NMESH', length=3, dtype=int)
-        _ = self._check_array_or_scalar('LBOX', length=3)
-        _ = self._check_array_or_scalar('PERIODIC', length=3, dtype=int)
-        _ = self._check_array_or_scalar('COI', length=3)
-        _ = self._check_scalar('LPTORDER', dtype=int)
+        self._check_scalar('NMESH', dtype=int)
+        self._check_array_or_scalar('LBOX', length=3)
+        self._check_array_or_scalar('PERIODIC', length=3, dtype=int)
+        self._check_array_or_scalar('COI', length=3)
+        self._check_scalar('LPTORDER', dtype=int)
 
-        _ = self._check_scalar('R_3D')
-        _ = self._check_scalar('D_4D')
-        _ = self._check_scalar('NRBINS', dtype=int)
+        self._check_scalar('REDSHIFT')
+        self.P['SCALE'] = 1.0 / (self.P['REDSHIFT'] + 1.0)
 
-        GEOMETRY = self._check_string('GEOMETRY')
-        if GEOMETRY not in ['cylindrical', 'spherical', 'cubical']:
-            raise ValueError(f'Error: unknown geometry `{GEOMETRY}`!\nExiting.')
-        BIN_MODE = self._check_string('BIN_MODE')
-        if BIN_MODE not in ['omega', 'volume']:
-            raise ValueError(f'Error: unknown binning mode `{BIN_MODE}`!\nExiting.')
+        self._check_scalar('R_3D')
+        self._check_scalar('D_4D')
+        self._check_scalar('NRBINS', dtype=int)
+        self._check_string('GEOMETRY')
+        if self.P['GEOMETRY'] not in ['cylindrical', 'spherical', 'cubical']:
+            raise ValueError(f"Error: unknown geometry `{self.P['GEOMETRY']}`!\nExiting.")
+        self._check_string('BIN_MODE')
+        if self.P['BIN_MODE'] not in ['omega', 'volume']:
+            raise ValueError(f"Error: unknown binning mode `{self.P['BIN_MODE']}`!\nExiting.")
 
-        TYPE = self._check_string('TYPE')
-        if TYPE not in ['grid', 'random', 'glass']:
-            raise ValueError(f'Error: unknown IC pattern `{TYPE}`!\nExiting.')
+        self._check_string('TYPE')
+        if self.P['TYPE'] not in ['grid', 'random', 'glass']:
+            raise ValueError(f"Error: unknown IC pattern `{self.P['TYPE']}`!\nExiting.")
+        if self.P['TYPE'] == 'glass':
+            self._check_exists('INPUT_GLASS')
+            self._check_path('INPUT_GLASS')
+        
+        self._check_string('IC_PREFIX')
+        self._check_path('IC_DIR', makedir=True)
+        self._check_string('IC_FORMAT')
+        if self.P['IC_FORMAT'] not in ['ascii', 'npy', 'hdf5', 'gadget']:
+            raise ValueError(f"Error: unknown output file format `{self.P['IC_FORMAT']}`!\nExiting.")
+        self._check_boolean('USE_DOUBLE')
+        if self.P['USE_DOUBLE']:
+            self.P['DTYPE'] = np.float64
+        else:
+            self.P['DTYPE'] = np.float32
 
-        if TYPE == 'glass':
-            _ = self._check_exists('INPUT_GLASS')
-            _ = self._check_path('INPUT_GLASS')
-        _ = self._check_string('IC_PREFIX')
-        _ = self._check_path('IC_DIR', makedir=True)
-        IC_FORMAT = self._check_string('IC_FORMAT')
-        if IC_FORMAT not in ['ascii', 'npy', 'hdf5', 'gadget']:
-            raise ValueError(f'Error: unknown output file format `{IC_FORMAT}`!\nExiting.')
+        self._check_scalar('NPART', dtype=int)
+        self._check_scalar('SEED', dtype=int)
 
-        _ = self._check_scalar('NPART')
-        _ = self._check_scalar('SEED', dtype=int)
+        self._check_boolean('SPHEREMODE')
+        self._check_boolean('COMOVING')
+        self._check_boolean('HINDEPENDENT')
 
-        # Print initial condition parameters
+        self._check_boolean('COUNTER')
+        self._check_scalar('PHASE_SHIFT')
+
+        self._check_string('SPECTRUM')
+        if self.P['SPECTRUM'] not in ['input', 'camb']:
+            raise ValueError(f"Error: unknown spectrum type `{self.P['SPECTRUM']}`!\nExiting.")
+        if self.P['SPECTRUM'] == 'input':
+            self._check_path('INPUT_SPECTRUM')
+            self._check_scalar('INPUT_SPECTRUM_UNIT_L_IN_CM')
+
+        self._check_scalar('UNIT_L_IN_CM')
+        self._check_scalar('UNIT_M_IN_G')
+        self._check_scalar('UNIT_V_IN_CM_PER_S')
+
         text = dedent(f'''
         IC parameters
         -------------
@@ -183,6 +228,8 @@ class CosmoParameters:
         Mesh size:                     {self.P['NMESH']} voxels
         Box size:                      {self.P['LBOX']} Mpc/h
         Periodicity along x-y-z axis:  {self.P['PERIODIC']}
+        Target redshift:               {self.P['REDSHIFT']:.3f}
+        Target scale factor:           {self.P['SCALE']:.6f}
         Center of interest:            {self.P['COI']} Mpc/h
         Euclidean simulation radius:   {self.P['R_3D']} Mpc/h
         Compact. simulation diameter:  {self.P['D_4D']} Mpc/h
@@ -191,14 +238,7 @@ class CosmoParameters:
         IC output directory:           {self.P['IC_DIR']}
         IC name prefix:                {self.P['IC_PREFIX']}
         Comoving IC:                   {self.P['COMOVING']}
-        Counter phase simulation:      {self.P['COUNTER']:.2f}
+        Counter phase simulation:      {self.P['COUNTER']}
         Phase shift:                   {self.P['PHASE_SHIFT']:.2f} degrees
         ''')
         log.info(text)
-        # Supplementary log messages and operations
-        _ = self._check_scalar('UNIT_L_IN_CM')
-        _ = self._check_scalar('UNIT_M_IN_G')
-        _ = self._check_scalar('UNIT_V_IN_CM_PER_S')
-        # check of boolean
-        if self.P['COMOVING'] not in (0, 1):
-            raise ValueError('Error: the COMOVING parameter should be 1 or 0!\nExiting.')
