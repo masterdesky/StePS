6fd48b7796badd8ca3d43a562c7cfd4883c778f8
masterdesky <masterdesky@gmail.com>
2025-08-02 00:48:59 +0200

    header now prints correctly and incorporated in stepsic

--- StePS_IC/stepsic/__init__.py
+++ StePS_IC/stepsic/__init__.py
@@ -1,7 +1,60 @@
-"""
+'''
 StePS initial-condition generator package.
-"""
+'''
 
-__version__ = "2.0.0"
-__year__ = "2017-2025"
-__authors__ = ["Gabor Racz", "Balazs Pal"]
+from textwrap import dedent
+
+__version__ = '2.0.0'
+__year__ = '2017-2025'
+__authors__ = ['Gabor Racz', 'Balazs Pal']
+
+
+def _make_header(Nart: int = 79, Ncop: int = 79, Nwar: int = 79):
+    art = dedent(f'''
+    \t       _                 _      
+    \t      | |               (_)     
+    \t   ___| |_ ___ _ __  ___ _  ___ 
+    \t  / __| __/ _ \ '_ \/ __| |/ __|
+    \t  \__ \ ||  __/ |_) \__ \ | (__ 
+    \t  |___/\__\___| .__/|___/_|\___|
+    \t              | |               
+    \t              |_|                
+    stepsic {__version__}
+    \tAn IC generator python script for
+    \tSTEreographically Projected cosmological Simulations
+    ''')
+    # cop = dedent(f'''
+    # Copyright (C) ({__year__}) {', '.join(__authors__)}
+    # \tJet Propulsion Laboratory, California Institute of Technology | Pasadena, CA, USA
+    # \tDepartment of Physics of Complex Systems, Eotvos Lorand University | Budapest, Hungary
+    # \tHeavy-ion Physics Research Group, HUN-REN Wigner RCP | Budapest, Hungary
+    # \tDepartment of Physics & Astronomy, Johns Hopkins University | Baltimore, MD, USA
+    # \tDepartment of Physics, University of Helsinki | Helsinki, Finland
+    # ''')
+    cop = dedent(f'''
+    Copyright (C) ({__year__}) {', '.join(__authors__)}
+    - Jet Propulsion Laboratory, California Institute of Technology
+    - Department of Physics of Complex Systems, Eotvos Lorand University
+    - Heavy-ion Physics Research Group, HUN-REN Wigner RCP
+    - Department of Physics & Astronomy, Johns Hopkins University
+    - Department of Physics, University of Helsinki
+    ''')
+    war = dedent(f'''
+    StePS_IC.py comes with ABSOLUTELY NO WARRANTY.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions. See the LICENSE file for details.
+    ''')
+    # Define horizontal borders: +-- ... --+
+    b  = lambda N: f'+{"-"*(N)}+'
+    # Converts multiline string to list of lines
+    ls = lambda s: s.expandtabs(4).splitlines()[1:]
+    # Pad RHS of all lines with spaces to get them equally `N` chars wide
+    T  = lambda s, N: '\n'.join([f"| {l}{' '*(N-1-len(l))}|" for l in ls(s)])
+
+    return ('\n'.join([
+        f'{b(Nart)}\n{T(art, Nart)}\n{b(Nart)}',
+        f'{b(Ncop)}\n{T(cop, Ncop)}\n{b(Ncop)}',
+        f'{b(Nwar)}\n{T(war, Nwar)}\n{b(Nwar)}'
+    ]))
+
+__header__ = _make_header()
